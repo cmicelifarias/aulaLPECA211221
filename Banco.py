49de00b809ca3801ea3a329c5c7d3d75f07ae6a5
cmicelifarias <cmicelifarias@users.noreply.github.com>
2021-12-21 14:49:07 -0300

    Add files via upload

---
+++ Banco.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class Banco:
+    def __init__(self):
+        self.conexao = sqlite3.connect("banco.db")
+        self.createTable()
+
+    def createTable(self):
+        c = self.conexao.cursor()
+        #executa um comando SQL
+        #nesse caso cria uma tabela
+        c.execute(
+            """
+            create table if not exists usuarios(
+                idusuario integer primary key autoincrement,
+                nome text,
+                telefone text,
+                email text,
+                usuario text,
+                senha text
+            )
+            """
+        )
+        self.conexao.commit()
+        c.close()
---
+++ usuarios.py
@@ -0,0 +1,69 @@
+from Banco import Banco
+
+# ao tipo de sistema que 
+# cria um elemento -> insert
+#remove um elemento -> delete
+#update de um elemento -> update
+# busca um elemento -> select
+# dá-se o nome de CRUD
+
+class Usuario:
+
+    def __init__(self, idusuario=0, nome="",telefone="",email="", usuario="",senha=""):
+        self.info = {}
+        self.idusuario=idusuario
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.usuario = usuario
+        self.senha = senha
+    
+    def insertUser(self):
+
+        banco = Banco()
+
+        c = banco.conexao.cursor()
+        comando = "insert into usuarios(nome, telefone,email,usuario, senha) values('"+ self.nome +"', '"+ self.telefone + "','"+ self.email +"','"+ self.usuario +"','"+ self.senha +"')"
+        c.execute(comando)
+        banco.conexao.commit()
+        c.close()
+    
+    def deleteUser(self):
+        banco = Banco()
+
+        c = banco.conexao.cursor()
+        comando = "delete from usuarios where idusuario = "+self.idusuario+ " "
+        c.execute(comando)
+        banco.conexao.commit()
+        c.close()
+
+    def selectUser(self, id):
+        banco = Banco()
+
+        c = banco.conexao.cursor()
+        comando = "select * from usuarios where idusuario = " + id + " "
+        c.execute(comando)
+
+        # c recebe uma lista com os atributos
+        #exemplo ["2","claudio","1111","claudio@ufrj.br","claudio","123456"]
+
+        print (c) #só para debug depois
+
+        for elemento in c:
+            self.idusuario = elemento[0]
+            self.nome = elemento[1]
+            self.telefone = elemento[2]
+            self.email = elemento[3]
+            self.usuario = elemento[4]
+            self.senha = elemento[5]
+
+        c.close()
+
+    def updateUser(self):
+        banco = Banco()
+
+        c = banco.conexao.cursor()
+        comando = "update usuarios set nome = '" + self.nome +"', telefone = '" + self.telefone +"', email = '"+self.email+"', usuario = '" +self.usuario+"',senha = '"+self.senha+"'where idusuario = "+self.idusuario+""
+        c.execute(comando)
+        banco.conexao.commit()
+        c.close()
